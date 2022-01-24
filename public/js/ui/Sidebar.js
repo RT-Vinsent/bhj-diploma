399b9fc42dffcc74b694e1e0a5a2efc5e7bcd031
Roman T <rt.hagbo@gmail.com>
2022-01-24 16:39:04 +0500

    2.1 Кнопка управления боковой колонкой

--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -12,16 +12,23 @@ class Sidebar {
     this.initToggleButton();
   }
 
-  /**
+  /** (2.1) (6)
    * Отвечает за скрытие/показа боковой колонки:
    * переключает два класса для body: sidebar-open и sidebar-collapse
    * при нажатии на кнопку .sidebar-toggle
    * */
   static initToggleButton() {
+    let sidebarMini = document.querySelector('.sidebar-mini');
+    let sidebarToggle = sidebarMini.querySelector('.sidebar-toggle');
 
+    sidebarToggle.addEventListener('click', function(event) {
+      event.preventDefault();
+      sidebarMini.classList.toggle('sidebar-open');
+      sidebarMini.classList.toggle('sidebar-collapse');
+    });
   }
 
-  /**
+  /** (2.4) (9)
    * При нажатии на кнопку входа, показывает окно входа
    * (через найденное в App.getModal)
    * При нажатии на кнопку регастрации показывает окно регистрации
@@ -29,6 +36,24 @@ class Sidebar {
    * выходу устанавливает App.setState( 'init' )
    * */
   static initAuthLinks() {
+    document.querySelector('.menu-item_register').addEventListener('click', (event) => {
+      event.preventDefault();
+      App.getModal('register').open();
+    });
 
+    document.querySelector('.menu-item_login').addEventListener('click', (event) => {
+      event.preventDefault();
+      App.getModal('login').open();
+    });
+
+    document.querySelector('.menu-item_logout').addEventListener('click', (event) => {
+      event.preventDefault();
+      User.logout((err, response) => {
+        if (response.success) {
+          App.setState('init');
+        }
+      });
+      
+    });
   }
 }
